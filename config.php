c8f86a031d10ccf06a60add5de074ef3f4c92e8b
semiPlanet <planetsafi@gmail.com>
2020-04-21 09:10:12 +0545

    update

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class cleanto_myvariable{
-	public $hostnames = "localhost";
-	public $username = "safasewa_system";
-	public $passwords = "I]O^Gby?L@yV";
-	public $database = "safasewa_system";
-	public $epcode = "2988962c-a800-4e4d-971b-4df1edbd6834";
-	
-} ?>
